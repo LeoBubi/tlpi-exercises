83d3a3ef0125ae5fb3d9da0974a10a2d06be31bc
Leonardo Brugnano <lebrug504@gmail.com>
2024-12-15 18:39:00 +0100

    tee: Implemented

--- src/ch04/tee.c
+++ src/ch04/tee.c
@@ -12,12 +12,23 @@ already exists.
 #define _GNU_SOURCE 1
 #include <fcntl.h>
 #include <getopt.h>
+#include <limits.h>
 #include <stdbool.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
+#define READ_SIZE 1024
+
+#ifdef loop
+#  undef loop
+#endif
+#define loop while (true)
+
+bool verbose = false;
 
 #define help() do { \
     fprintf(stdout, \
@@ -25,6 +36,7 @@ already exists.
         "Copy standard input to each FILE, and also to standard output.\n" \
         "\n" \
         "  -a, --append     append to the given FILEs, do not overwrite\n" \
+        "  -v, --verbose    be verbose with errors\n" \
         "\n" \
         "    --help      display help and exit\n" \
         "    --version   output version information and exit\n" \
@@ -35,38 +47,94 @@ already exists.
 #define version() do { \
     fprintf(stdout, \
         PROGRAM_NAME" version "VERSION" ("RELEASE_DATE")\n" \
-        "Written by "AUTHORS); \
+        "Written by "AUTHORS"\n"); \
     exit(EXIT_SUCCESS); \
 } while(0)
 
+void error_exit(const char *format, ...) 
+{
+    fprintf(stderr, PROGRAM_NAME": ");
+    
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+
+    fprintf(stderr, ".\n");
+    if (verbose) perror(NULL);
+    exit(EXIT_FAILURE);
+}
+
+
 int main(int argc, char **argv)
 {
-    unsigned int oflag = O_TRUNC;
+    int oflag = O_TRUNC;
+
+    int fds[argc-1];
+    int fds_size;
+
+    char buffer[READ_SIZE];
+    ssize_t rb;
 
     /* read command-line options */
-    const char optstring[] = "a";
+
+    const char optstring[] = "av";
     const struct option longopts[] = {
         {"append", no_argument, NULL, 'a'},
-        {"help", no_argument, NULL, 'h'},
-        {"version", no_argument, NULL, 'v'},
+        {"verbose", no_argument, NULL, 'v'},
+        {"help", no_argument, NULL, INT_MAX-'h'},
+        {"version", no_argument, NULL, INT_MAX-'v'},
         {0, 0, 0, 0}
     };
 
     int optchar;
-    while ((optchar = getopt_long(argv, argv, optstring, longopts, NULL)) == -1)
+    while ((optchar = getopt_long(argc, argv, optstring, longopts, NULL)) != -1)
         switch (optchar) {
             case 'a':
                 oflag = O_APPEND;
                 break;
-            case 'h':
-                help();
             case 'v':
+                verbose = true;
+                break;
+            case (INT_MAX-'h'):
+                help();
+            case (INT_MAX-'v'):
                 version();
-            default:
+            case '?':
                 fprintf(stderr, 
                     PROGRAM_NAME": invalid option -- '%c'\n"
-                    "Try '"PROGRAM_NAME" --help' for more information"
+                    "Try '"PROGRAM_NAME" --help' for more information\n"
                     , (char)optopt);
                 exit(EXIT_FAILURE);
+            default:
+                break;
         }
+    
+    /* open files for writing */
+    
+    fds[0] = STDOUT_FILENO;
+    fds_size = 1;
+    
+    for (int i = optind; i < argc; i++, fds_size++)
+        if ((fds[fds_size] = open(argv[i], O_WRONLY | O_CREAT | oflag, 0640)) == -1)
+            error_exit("Unable to open file '%s' for writing", argv[i]);
+
+    /* start copying */
+
+    loop {
+        if ((rb = read(STDOUT_FILENO, buffer, READ_SIZE)) == -1)
+            error_exit("Error reading standard input");
+        
+        if (!rb) break;
+
+        for (int i = 0; i < fds_size; i++) {
+            if (write(fds[i], buffer, rb) == -1)
+                error_exit("Error writing to file");
+        }
+    }
+
+    for (int i = 1; i < fds_size; i++) {
+        if (close(fds[i]) == -1 && verbose)
+            perror(PROGRAM_NAME": close");
+    }
 }
